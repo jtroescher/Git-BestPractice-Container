0c0de14c83a8f74cf360bb828eaa0a8b4e8b6192
JTroescher <jacobtroescher6@gmail.com>
2023-11-09 20:52:16 -0700

    updating files

--- src.py
+++ src.py
@@ -1,6 +1,4 @@
-from typing import List
-
-def calculate_average(n: int, t: List[int]) -> int:
+def calculate_average(n, t):
     if n == 0:
         return 0  # Avoid division by zero
     return sum(t) // n
@@ -8,7 +6,7 @@ def calculate_average(n: int, t: List[int]) -> int:
 
 # Main block
 if __name__ == "__main__":
-    n: int = int(input())
-    t: List[int] = list(map(int, input().split()))
-    average: int = calculate_average(n, t)
+    n = int(input())
+    t = list(map(int, input().split()))
+    average = calculate_average(n, t)
     print(average)
