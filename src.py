c06c1897b7d92201f635103905bed3e24e978704
JTroescher <jacobtroescher6@gmail.com>
2023-11-09 20:29:36 -0700

    updating src

--- src.py
+++ src.py
@@ -3,7 +3,7 @@ def calculate_average(n, t):
         return 0  # Avoid division by zero
     return sum(t) // n
 
-# noqa: E305 (expected 2 blank lines after function definition)
+# Main block
 if __name__ == "__main__":
     n = int(input())
     t = list(map(int, input().split()))
