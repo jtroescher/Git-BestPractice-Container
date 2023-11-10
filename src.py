dee63e9918a024efbf22e2f268af59a32e607447
JTroescher <jacobtroescher6@gmail.com>
2023-11-09 21:09:47 -0700

    Updating files

--- src.py
+++ src.py
@@ -1,8 +1,9 @@
-def calculate_average(n, t):
+from typing import List
+
+def calculate_average(n: int, t: List[int]) -> int:
     if n == 0:
         return 0  # Avoid division by zero
     return sum(t) // n
-
 
 # Main block
 if __name__ == "__main__":
